222a907fbe34ed23154f51bc685df1ffa3f86f8b
Anvar <anvartk@gmail.com>
2017-01-19 15:23:33 +0530

    Added sidebar background color changing and topbar background color changing options.

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,5 +1,199 @@
 "use strict";
 
+
+$().ready(function(){
+    $sidebar = $('.sidebar');
+    var $sidebar_img_container = $sidebar.find('.sidebar-background');
+
+    var $full_page = $('.full-page');
+
+    var $sidebar_responsive = $('body > .navbar-collapse');
+
+    var window_width = $(window).width();
+
+    var fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
+
+    if( window_width > 767 && fixed_plugin_open == 'Dashboard' ){
+        if($('.fixed-plugin .dropdown').hasClass('show-dropdown')){
+            $('.fixed-plugin .dropdown').addClass('open');
+        }
+
+    }
+
+    $('.fixed-plugin a').click(function(event){
+        	console.log('33333');
+
+      // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
+        if($(this).hasClass('switch-trigger')){
+            if(event.stopPropagation){
+                event.stopPropagation();
+            }
+            else if(window.event){
+               window.event.cancelBubble = true;
+            }
+        }
+    });
+
+    $('.fixed-plugin .active-color span').click(function(){
+    
+    
+        var $topbar_background = $('.navbar');
+
+        var new_color = $(this).data('color');
+        
+        $topbar_background.attr('data-topbar-color',new_color);
+        
+    /*
+        $full_page_background = $('.full-page-background');
+
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        var new_color = $(this).data('color');
+
+        if($sidebar.length != 0){
+            $sidebar.attr('data-active-color',new_color);
+        }
+
+        if($full_page.length != 0){
+            $full_page.attr('filter-color',new_color);
+        }
+
+        if($sidebar_responsive.length != 0){
+            $sidebar_responsive.attr('data-color',new_color);
+        }
+        
+        */
+    });
+
+    $('.fixed-plugin .background-color span').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        var new_color = $(this).data('color');
+
+        if($sidebar.length != 0){
+            $sidebar.attr('data-background-color',new_color);
+        }
+    });
+
+    $('.fixed-plugin .img-holder').click(function(){
+        $full_page_background = $('.full-page-background');
+
+        $(this).parent('li').siblings().removeClass('active');
+        $(this).parent('li').addClass('active');
+
+
+        var new_image = $(this).find("img").attr('src');
+
+        if( $sidebar_img_container.length !=0 && $('.switch-sidebar-image input:checked').length != 0 ){
+            $sidebar_img_container.fadeOut('fast', function(){
+               $sidebar_img_container.css('background-image','url("' + new_image + '")');
+               $sidebar_img_container.fadeIn('fast');
+            });
+        }
+
+        if($full_page_background.length != 0 && $('.switch-sidebar-image input:checked').length != 0 ) {
+            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
+
+            $full_page_background.fadeOut('fast', function(){
+               $full_page_background.css('background-image','url("' + new_image_full_page + '")');
+               $full_page_background.fadeIn('fast');
+            });
+        }
+
+        if( $('.switch-sidebar-image input:checked').length == 0 ){
+            var new_image = $('.fixed-plugin li.active .img-holder').find("img").attr('src');
+            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
+
+            $sidebar_img_container.css('background-image','url("' + new_image + '")');
+            $full_page_background.css('background-image','url("' + new_image_full_page + '")');
+        }
+
+        if($sidebar_responsive.length != 0){
+            $sidebar_responsive.css('background-image','url("' + new_image + '")');
+        }
+    });
+
+    $('.switch-sidebar-image input').change(function(){
+
+        var $full_page_background = $('.full-page-background');
+
+        var $input = $(this);
+        var background_image;
+
+        if($input.is(':checked')){
+            if($sidebar_img_container.length != 0){
+                $sidebar_img_container.fadeIn('fast');
+                $sidebar.attr('data-image','#');
+            }
+
+            if($full_page_background.length != 0){
+                $full_page_background.fadeIn('fast');
+                $full_page.attr('data-image','#');
+            }
+
+            background_image = true;
+        } else {
+            if($sidebar_img_container.length != 0){
+                $sidebar.removeAttr('data-image');
+                $sidebar_img_container.fadeOut('fast');
+            }
+
+            if($full_page_background.length != 0){
+                $full_page.removeAttr('data-image','#');
+                $full_page_background.fadeOut('fast');
+            }
+
+            background_image = false;
+        }
+    });
+
+    $('.switch-sidebar-mini input').change(function(){
+        var $body = $('body');
+
+        var $input = $(this);
+
+        if(md.misc.sidebar_mini_active == true){
+            $('body').removeClass('sidebar-mini');
+            md.misc.sidebar_mini_active = false;
+
+            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+
+        }else{
+
+            $('.sidebar .collapse').collapse('hide').on('hidden.bs.collapse',function(){
+                $(this).css('height','auto');
+            });
+
+            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar('destroy');
+
+            setTimeout(function(){
+                $('body').addClass('sidebar-mini');
+
+                $('.sidebar .collapse').css('height','auto');
+                md.misc.sidebar_mini_active = true;
+            },300);
+        }
+
+        // we simulate the window Resize so the charts will get updated in realtime.
+        var simulateWindowResize = setInterval(function(){
+            window.dispatchEvent(new Event('resize'));
+        },180);
+
+        // we stop the simulation of Window Resize after the animations are completed
+        setTimeout(function(){
+            clearInterval(simulateWindowResize);
+        },1000);
+
+    });
+
+
+
+
+});
+
+
 var demo = {
     initPickColor: function(){
         $('.pick-class-label').on('click',function(){
